8ef27139e747b30d522bf9846ada98ceecac8f9a
Steve Bagwell <baggerone@gmail.com>
2016-05-24 16:16:20 -0400

    set ownership and permissions on cert files

--- run_ldap.sh
+++ run_ldap.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+
+
 mkdir -p /var/lib/ldap/accesslog 
 chown ldap:ldap /var/lib/ldap/accesslog  
+
+chown ldap:ldap /etc/openldap/certs/idp.pw.api.*
+chmod 600 /etc/openldap/certs/idp.pw.api.*
 
 exec /usr/sbin/slapd -h "ldap:/// ldaps:/// ldapi:///" -u ldap -d $DEBUG_LEVEL -f /etc/openldap/slapd.conf
